64bd6b8df482d38ef7014bb75b68d987a6a079de
bora <baktas19@ku.edu.tr>
2023-01-05 00:03:01 +0300

    TimeWasterAlien is interested with building time not avatars

--- src/domain/gameObjects/alien/TimeWasterAlien.java
+++ src/domain/gameObjects/alien/TimeWasterAlien.java
@@ -44,11 +44,12 @@ public class TimeWasterAlien extends Alien{
         }
     }
 
-    public void updateBehavior()
-    {
-        Avatar avatar= BuildingTracker.getBuildingList().get(BuildingTracker.getCurrentIndex()).getAvatar();
-        double percentage = avatar.getCurrentTime()/avatar.getTime() * 100;
-        if (percentage >=70)
+    public void updateBehavior() {
+
+        Building building = BuildingTracker.getBuildingList().get(BuildingTracker.getCurrentIndex());
+        double percentage = (building.getTime()/(5 * building.getNumofObstacles(building.getMap_obj()))) * 100;
+
+        if (percentage >= 70)
         {
             setBehavior(new TimeWasteHard(this));
         }
