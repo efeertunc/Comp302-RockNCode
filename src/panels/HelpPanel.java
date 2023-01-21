8dbaa2ab412510eba4c43e840ddae6edf7eb6c31
efeertunc <eertunc19@ku.edu.tr>
2023-01-21 23:35:59 +0300

    feat: help text

--- src/panels/HelpPanel.java
+++ src/panels/HelpPanel.java
@@ -79,14 +79,20 @@ public class HelpPanel implements IPanel {
 		textPane.setBackground(new Color(255, 255, 204));
 		textPane.setBounds(90, 100, 800, 350);
 		textPane.setText(
-				"Building mode -> \n" +
-						"1.Click to map to insert obstacles\n" +
-						"2.After that you can continue to other buildings and start the running mode key will not be visible, player will search for it under the obstacles."
-						+ " Running mode -> Main obcejtive is to find the key and pass the door. Time will be renewed in the new building. \n"
-						+ " Powerups : There are 5 types of powerups that can be clicked and collected during the running mode. First one is Vest power up, it guards the player 15 seconds long and avoids player from loosing his/her life."
-						+ " To use this power-up click “V”. Hint power up can be used by clicking “H” key, it will immediately show the place of the key. Extra life power up directly adds 1 life to the player lives.  "
-						+ " Extra time powerup is also directly added to the players time. Last one is bottle, avatar holds the bottle ones the “B” key clicked, then gives a direction to throw."
-						);
+				"Building mode ->   Click to map to insert obstacles. After that you can continue to other " +
+						"buildings and start the running mode." +
+						"\n" + "Running mode -> Main objective is to find the key and pass the door. " +
+						"Time will be renewed in the each building accordingly number of obstacles. \n"
+						+"Powerups -> There are 5 types of powerups that can be clicked and collected during the running mode. First one is " +
+						"Vest power up, it guards the player 15 seconds long and avoids player from loosing his/her life. To use this power-up click “V”. Hint power up can be used by clicking " +
+						"“H” key, it will immediately show the place of the key. Extra life power up directly adds 1 life to the player lives. Extra time powerup is also directly" +
+						" added to the players time. Last one is bottle, avatar holds"
+						+"the bottle ones the B key clicked, then gives a direction to throw. \n" +
+						"Aliens ->  Shooter alien appears in random locations in the building " +
+						" and shoots a bullet every second. Blind alien tries to kill the player, " +
+						"it randomly walks. Time wasting alien changes location of the key every 5 seconds. \n"
+
+		);
 		panel.add(textPane);
 	}
 
